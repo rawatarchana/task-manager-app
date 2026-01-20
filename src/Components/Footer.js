4b7f9106a32b66e7ffc98dde5f76d5044f80238d
Archana Rawat <114794256+rawatarchana@users.noreply.github.com>
2026-01-20 00:30:08 +0000

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,10 @@
 const Footer = () => {
     return(
         <footer className="text-center py-2">
-            <h6>&copy; EdFRAX {new Date().getFullYear()}</h6>
+            <h6>&copy; Archana {new Date().getFullYear()}</h6>
         </footer>
     )
 }
+
 
 export default Footer;
